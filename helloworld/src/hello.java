0e81d1b1d43b1342e3f2dc56003727c4a87e1cfd
mitchtz <mitchtz@users.noreply.github.com>
2014-09-17 13:44:08 -0600

    Update hello.java

--- helloworld/src/hello.java
+++ helloworld/src/hello.java
@@ -2,6 +2,6 @@
 public class hello {
 	public static void main(String[] arsg){
 		System.out.println("Hello World!");
-		System.out.println("This is he second line");
+		System.out.println("This is he second line!");
 	}
 }
